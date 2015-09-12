711f9fbfc60ff360199afbb87c961e8b31f4cf58
Bogdan Heim <heim.bogdan@hotmail.com>
2015-09-12 21:38:21 +0300

    a

--- Invoicer/src/test/MainTest.java
+++ Invoicer/src/test/MainTest.java
@@ -1,8 +1,14 @@
 package test;
 
+import org.hibernate.boot.model.relational.Database;
+
+import ro.h.service.database.connection.DataBaseConnector;
+
 public class MainTest {
 
 	public static void main(String[] args) {
 		
+		DataBaseConnector connector = DataBaseConnector.getInstance();
+		connector.getSession();
 	}
 }
